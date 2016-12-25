abe856e0afdb80eb343a13ef277b6f4fac084eb2
Yu Kitano <toxicelda@gmail.com>
2016-12-25 20:37:28 +0900

    Initial commit

---
+++ kcuc/Classes/ViewControllers/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  kcuc
+//
+//  Created by Yuki on 2016/12/25.
+//  Copyright © 2016 Pushme Studio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
