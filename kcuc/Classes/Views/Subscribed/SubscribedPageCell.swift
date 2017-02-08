de4f0dd3edca46f7a0d77af58c7038b93d3d05ea
Yu Kitano <toxicelda@gmail.com>
2017-02-08 22:16:35 +0900

    Add updated time to Subscribed page list

--- kcuc/Classes/Views/Subscribed/SubscribedPageCell.swift
+++ kcuc/Classes/Views/Subscribed/SubscribedPageCell.swift
@@ -11,8 +11,18 @@ import CocoaLumberjackSwift
 
 class SubscribedPageCell: UITableViewCell {
   @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var updateDateLabel: UILabel!
   
-  func set(page:Page) {
+  func set(page: Page) {
     titleLabel.text = page.prodName
+    
+    if let date = page.updatedTime {
+      let dateFormatter: DateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "yyyy/MM/dd hh:mm:ss"
+      
+      let formattedDate = dateFormatter.string(from: date)
+      
+      updateDateLabel.text = "Updated: \(formattedDate)"
+    }
   }
 }
